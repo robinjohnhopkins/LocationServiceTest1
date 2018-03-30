c396a4ffa0352bf535f672ccf5e916461becccfe
Robin John Hopkins <robinjohnhopkins@Robins-MacBook-Pro.local>
2018-03-30 10:17:51 +0100

    post added

--- app/src/main/java/com/javacodegeeks/android/locationservicetest/MainActivity.java
+++ app/src/main/java/com/javacodegeeks/android/locationservicetest/MainActivity.java
@@ -7,6 +7,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -15,6 +16,15 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends Activity {
 	  private TextView latitude;
@@ -27,6 +37,7 @@ public class MainActivity extends Activity {
 	  private String provider;
 	  private MyLocationListener mylistener;
 	  private Criteria criteria;
+	  private PostClass postclass;
 	  
 	/** Called when the activity is first created. */
 
@@ -40,7 +51,7 @@ public class MainActivity extends Activity {
 		  choice = (TextView) findViewById(R.id.choice);
 		  fineAcc = (CheckBox) findViewById(R.id.fineAccuracy);
 		  choose = (Button) findViewById(R.id.chooseRadio);
-
+          postclass = null;
 		  // Get the location manager
 		  locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		  // Define the criteria how to select the location provider
@@ -58,7 +69,9 @@ public class MainActivity extends Activity {
 				 }else {
 					 criteria.setAccuracy(Criteria.ACCURACY_COARSE);
 					 choice.setText("coarse accuracy selected");
-				 }  
+				 }
+				 postclass = new PostClass();
+                 postclass.execute();
 			}
 		  });
 		  criteria.setCostAllowed(false); 
@@ -113,4 +126,57 @@ public class MainActivity extends Activity {
 		        Toast.LENGTH_SHORT).show();
 		  }
 	  }
+
+	private class PostClass extends AsyncTask<String, Void, Void> {
+
+		@Override
+		protected Void doInBackground(String... params) {
+			try {
+
+				final TextView outputView = (TextView) findViewById(R.id.choice);
+				URL url = new URL("https://4b4f0026.ngrok.io/postposition");
+
+				HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+				//String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+				String urlParameters = "lat=0.123456&lon=52.12345";
+				connection.setRequestMethod("POST");
+				connection.setRequestProperty("USER-AGENT", "Mozilla/5.0");
+				connection.setRequestProperty("ACCEPT-LANGUAGE", "en-US,en;0.5");
+				connection.setDoOutput(true);
+				DataOutputStream dStream = new DataOutputStream(connection.getOutputStream());
+				dStream.writeBytes(urlParameters);
+				dStream.flush();
+				dStream.close();
+				int responseCode = connection.getResponseCode();
+				final StringBuilder output = new StringBuilder("Request URL " + url);
+				output.append(System.getProperty("line.separator") + "Request Parameters " + urlParameters);
+				output.append(System.getProperty("line.separator")  + "Response Code " + responseCode);
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				String line = "";
+				StringBuilder responseOutput = new StringBuilder();
+				while((line = br.readLine()) != null ) {
+					responseOutput.append(line);
+				}
+				br.close();
+
+				output.append(System.getProperty("line.separator") + "Response " + System.getProperty("line.separator") + System.getProperty("line.separator") + responseOutput.toString());
+
+				MainActivity.this.runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						outputView.setText(output);;
+
+					}
+				});
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
 }
